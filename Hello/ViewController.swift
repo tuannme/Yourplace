eb65a368c6a4117ffae29f8f8321822d766cc64e
nguyen.manh.tuanb <nguyen.manh.tuanb@framgias-Mac-mini.local>
2018-04-12 09:27:17 +0700

    sdad

---
+++ Hello/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello
+//
+//  Created by nguyen.manh.tuanb on 4/12/18.
+//  Copyright © 2018 nguyen.manh.tuanb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
